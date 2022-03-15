2bdd0e36277db65497f277bcef33ff8daeab8194
Magnus Karström <magnus@yesweb.se>
2022-03-15 08:04:58 +0100

    Created with tutorial from Traversy media

---
+++ script.js
@@ -0,0 +1,29 @@
+const conatiner = document.getElementById('container')
+const colors = ['red','green','blue','white','yellow']
+const SQUARES = 400;
+
+for(let i = 0; i < SQUARES; i++){
+    const square = document.createElement('div')
+    square.classList.add('square')
+square.addEventListener('mouseover', () => setColor(square))
+
+square.addEventListener('mouseout', () => removeColor(square))
+
+
+    conatiner.appendChild(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+  element.style.background = color
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}` 
+}
+
+function removeColor(element){
+element.style.backgroundColor = `#1d1d1d`
+element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
