73434c4934c78e415ffbcad639b78ffb2e295258
Magnus Karström <magnus@yesweb.se>
2022-03-24 08:46:01 +0100

    Hoverboard is red

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const conatiner = document.getElementById('container')
-const colors = ['red']
+const color = 'red'
 const SQUARES = 500
 // ,'green','blue','white','yellow'
+
+
 for(let i = 0; i < SQUARES; i++){
     const square = document.createElement('div')
     square.classList.add('square')
+
 square.addEventListener('mouseover', () => setColor(square))
 
 square.addEventListener('mouseout', () => removeColor(square))
@@ -14,7 +17,7 @@ square.addEventListener('mouseout', () => removeColor(square))
 }
 
 function setColor(element) {
-    const color = getRandomColor()
+    
   element.style.background = color
   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}` 
 }
@@ -22,8 +25,4 @@ function setColor(element) {
 function removeColor(element){
 element.style.backgroundColor = `#1d1d1d`
 element.style.boxShadow = `0 0 2px #000`
-}
-
-function getRandomColor() {
-    return colors[Math.floor(Math.random() * colors.length)]
 }
